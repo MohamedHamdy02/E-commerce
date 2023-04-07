02dc547f732dd9de1330d11ac1f73942997ff8cf
MohamedHamdy02 <mohamedghazy900@gmail.com>
2023-04-07 23:44:35 +0200

    Added LocalStorage to the app

--- e-commerce/components/Navbar.jsx
+++ e-commerce/components/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return <>
-  <div className="navbar-container">
+    <div className="navbar-container">
       <p className="logo">
         <Link href="/">Ghazy Headphones</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className="cart-item-qty">{totalQuantities}</span>
+        <span suppressHydrationWarning className="cart-item-qty">{totalQuantities}</span>
       </button>
 
       {showCart && <Cart />}
